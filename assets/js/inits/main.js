2147a6d38fcda722c29bae4031dee482a1b46345
Edgar Contreras <edgar@nochesdecafeina.com>
2015-06-28 12:08:34 +0200

    Added Parse

--- assets/js/inits/main.js
+++ assets/js/inits/main.js
@@ -1,4 +1,5 @@
 $(function () {
+    Parse.initialize("dS3rAMmDvIsvWkBWyS9txTRlSNYPpIFboZ8eRReK", "NG3s7U0uZOgtD6pwF2g52okwoGKTBhvnMzVSF2yz");
     var slider = $("#slider");
     
     function autoScrollTo(sel,offset,fn) {
@@ -37,5 +38,24 @@ $(function () {
         $('.wm-search-wrapper').animate({ padding: 0 }, 400);
         $('.navbar').fadeIn();
         $('.logo__container').animate({ height: 0 }, 600);
+    });
+
+
+    var tourRequest = Parse.Object.extend("TourRequest");
+    var query = new Parse.Query(tourRequest);
+    
+    // Requests
+    query.find({
+      success: function(results) {
+        alert("Successfully retrieved " + results.length + " scores.");
+        // Do something with the returned Parse.Object values
+        // for (var i = 0; i < results.length; i++) {
+        //   var object = results[i];
+        //   alert(object.id + ' - ' + object.get('playerName'));
+        // }
+      },
+      error: function(error) {
+        alert("Error: " + error.code + " " + error.message);
+      }
     });
 });
