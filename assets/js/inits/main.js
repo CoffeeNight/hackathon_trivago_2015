5f969e90804070b9f10587e540e8fad2eb08b5e7
Edgar Contreras <edgar@nochesdecafeina.com>
2015-06-28 02:11:43 +0200

    Added some filters

--- assets/js/inits/main.js
+++ assets/js/inits/main.js
@@ -1,3 +1,14 @@
 $(function () {
     $('#datetimepicker1').datetimepicker();
+    $("#slider").noUiSlider({
+        start: [1, 200],
+        connect: true,
+        range: {
+            'min': 1,
+            'max': 1000
+        },
+        step: 1
+    });
+    $('#slider').Link('upper').to($('#slider-value-upper'));
+    $('#slider').Link('lower').to($('#slider-value-lower'));
 });
