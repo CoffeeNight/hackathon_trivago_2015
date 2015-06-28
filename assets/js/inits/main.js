e7f28e7c22c8148f46e80370074416b993cfb2a7
Edgar Contreras <edgar@nochesdecafeina.com>
2015-06-28 06:42:12 +0200

    Cleaned a little bit the JS structure

--- assets/js/inits/main.js
+++ assets/js/inits/main.js
@@ -1,11 +1,13 @@
-function autoScrollTo(sel,offset,fn) {
-	offset = offset || 0;
-    var top = $(sel).offset().top + offset;
-    $("html, body").animate({ scrollTop: top }, 1000, fn);
-}
-
 $(function () {
-    $("#slider").noUiSlider({
+    var slider = $("#slider");
+    
+    function autoScrollTo(sel,offset,fn) {
+        offset = offset || 0;
+        var top = $(sel).offset().top + offset;
+        $("html, body").animate({ scrollTop: top }, 1000, fn);
+    }
+
+    slider.noUiSlider({
         start: [1, 200],
         connect: true,
         range: {
@@ -14,24 +16,24 @@ $(function () {
         },
         step: 1
     });
-    $('#slider').Link('upper').to($('#slider-value-upper'));
-    $('#slider').Link('lower').to($('#slider-value-lower'));
+    slider.Link('upper').to($('#slider-value-upper'));
+    slider.Link('lower').to($('#slider-value-lower'));
     $('#datetimepicker1').datetimepicker({
-    	format: 'YYYY-MM-DD',
-    	widgetPositioning: {
-    	    horizontal: 'auto',
-	        vertical: 'bottom'
-    	}
+        format: 'YYYY-MM-DD',
+        widgetPositioning: {
+            horizontal: 'auto',
+            vertical: 'bottom'
+        }
     });
-    $('.home-seach-submit').click(function(e){
-    	e.preventDefault();
-    	$('.wm-big-header__image').animate({ height: 0 }, 1000);
-    	$('.js-results').animate({ opacity: 1 }, 700);
-    	$('.wm-search-wrapper').animate({ padding: 0 }, 700);
-    	$('.navbar').fadeIn();
-    	$('.logo__container').animate({ height: 0 }, 1000);
-    	// autoScrollTo('.wm-search-small-wrapper',-75,function(){
-    	// 	//$('.logo__container').hide();
-    	// });
-    })
+    $('.home-seach-submit').on('click', function(e){
+        e.preventDefault();
+        $('.wm-big-header__image').animate({ height: 0 }, 1000);
+        $('.js-results').animate({ opacity: 1 }, 700);
+        $('.wm-search-wrapper').animate({ padding: 0 }, 700);
+        $('.navbar').fadeIn();
+        $('.logo__container').animate({ height: 0 }, 1000);
+        // autoScrollTo('.wm-search-small-wrapper',-75,function(){
+        ////$('.logo__container').hide();
+        // });
+    });
 });
